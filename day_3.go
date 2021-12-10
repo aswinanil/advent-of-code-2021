368dc50c32dff5d06b170276e959739d7ba5a50a
Aswin Anil <mraswinanil@gmail.com>
2021-12-11 00:35:29 +0800

    add solution for day 3, part 2

--- day_3.go
+++ day_3.go
@@ -32,10 +32,62 @@ func getGammaAndEpilson(report []string) (string, string) {
     return gamma, epilson
 }
 
+func getFilteredReport(filter string, i int, report []string) []string {
+    var filteredReport []string
+
+    for _, line := range report {
+        if GetCharAt(line, i) == filter {
+            filteredReport = append(filteredReport, line)
+        }
+    }
+
+    return filteredReport
+}
+
+func getFilter(isOxygen bool, zeroCount int, oneCount int) string {
+    if isOxygen {
+        if zeroCount > oneCount {
+            return "0"
+        } else {
+            return "1"
+        }
+    } else {
+        if oneCount < zeroCount {
+            return "1"
+        } else {
+            return "0"
+        }
+    }
+}
+
+func getOxygenOrCO2Rating(report []string, isOxygen bool) string {
+    for i := 0; i < len(report[0]) && len(report) > 1; i++ {
+        oneCount, zeroCount := 0, 0
+
+        for j := 0; j < len(report); j++ {
+            if GetCharAt(report[j], i) == "0" {
+                zeroCount++
+            } else {
+                oneCount++
+            }
+        }
+
+        filter := getFilter(isOxygen, zeroCount, oneCount)
+        report = getFilteredReport(filter, i, report)
+    }
+
+    return report[0]
+}
+
 func main() {
     lines := ParseFile(3, false)
 
     // Part 1
     gamma, epilson := getGammaAndEpilson(lines)
     fmt.Println(GetIntFromBinary(gamma) * GetIntFromBinary(epilson))
+
+    // Part 2
+    oxygen := getOxygenOrCO2Rating(lines, true)
+    co2 := getOxygenOrCO2Rating(lines, false)
+    fmt.Println(GetIntFromBinary(oxygen) * GetIntFromBinary(co2))
 }
